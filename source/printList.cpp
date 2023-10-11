76bacf41670598063fd83bc7c5a7513f099013bb
Kacper Baryłowicz <112568080+malybaryl@users.noreply.github.com>
2023-10-11 14:22:16 +0200

    Add files via upload

--- source/printList.cpp
+++ source/printList.cpp
@@ -6,45 +6,31 @@ void printList(std::vector<std::string> drewNames)
     bool loop;
     bool correct;
     int name;
-    std::vector<int> nameWas;
+    int counter;
+    std::vector<int> nameToPrint;
     srand(time(NULL));
-    while(true)
+    
+    for (int i = 0; i < drewNames.size(); i+2)
     {
-    name = rand() % drewNames.size();
-    if (name%2==0)
-    {
-        nameWas.push_back(name);
-        break;
+        nameToPrint.push_back(i);
     }
-    }
-    for(int i=0;i<(drewNames.size()/2)-1;i++)
+    
+    counter = nameToPrint.size();
+
+    for (int i = 0; i<nameToPrint.size(); i++)
     {
-        loop = true;
-        while(loop==true)
-        {
-            name = rand() % drewNames.size();
-            if (name%2==0)
-            {
-                correct=true;
-                for(int j=0; j<nameWas.size() ; j++)
-                {
-                    if(name==nameWas[j])
-                    {
-                        correct=false;
-                    }
-                }
-                if(correct==true)
-                {
-                    nameWas.push_back(name);
-                    loop=false;
-                }
-            }
-        }
-    }
-   
-    for(int i=0 ; i<nameWas.size() ; i++)
-    {
-        std::cout << nameWas[i] << " ";
+        name = rand() % counter;
+        counter--;
+        std::cout << "Get ready: " << drewNames[nameToPrint[name]] << std::endl << std::endl 
+                  << "Type anything to continue: ";
+
+        std::cin >> nothing;
+        
+        std::cout << "Your person is: " << drewNames[nameToPrint[name]+1] << std::endl << std::endl 
+                  << "Type anything to continue: ";
+
+        std::cin >> nothing;
+        nameToPrint.erase(nameToPrint.begin()+name-1);
     }
        
 }
